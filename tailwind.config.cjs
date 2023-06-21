32c72b451a90b659b95ccd87282bd4f5bf9b9e89
tohbin02 <fanfan8859@gmail.com>
2023-06-21 12:06:12 +0800

    chore: add tailwind and config font

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+import { defineConfig } from 'astro/config';
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily:{
+				'sans': ['Poppins', ...defaultTheme.fontFamily.sans],
+			}
+		},
+	},
+	plugins: [],
+}
